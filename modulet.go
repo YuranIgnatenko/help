2746fd23228eaa6ef86162f4cbcb488cbe00af24
Юрий <90733093+YuranIgnatenko@users.noreply.github.com>
2022-05-17 00:12:53 +0300

    Add files via upload

---
+++ modulet.go
@@ -0,0 +1,35 @@
+package modulet
+
+import "fmt"
+
+type Cat struct {
+	name string
+}
+
+func (cat *Cat) Say() {
+	fmt.Println("Meow")
+}
+
+func (cat *Cat) GetName() string {
+	return cat.name
+}
+
+func (cat *Cat) SetName(name string) {
+	cat.name = name
+}
+
+type Dog struct {
+	name string
+}
+
+func (dog *Dog) Say() {
+	fmt.Println("Awf")
+}
+
+func (dog *Dog) GetName() string {
+	return dog.name
+}
+
+func (dog *Dog) SetName(name string) {
+	dog.name = name
+}
